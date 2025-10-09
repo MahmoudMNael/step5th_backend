b0c55fe37eb5188f5e6fafaa8f978959693248e4
mahmoudmnael <mahmoudmnael@gmail.com>
2025-10-09 11:03:45 +0300

    feat: reorganize module imports in AppModule for better structure and clarity

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,9 +12,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
 import { AuthModule } from './auth/auth.module';
+import { CashoutModule } from './cashout/cashout.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ConnectionsModule } from './connections/connections.module';
 import { FilesModule } from './files/files.module';
+import { OrdersModule } from './orders/orders.module';
 import { PaymobModule } from './paymob/paymob.module';
 import { PlansModule } from './plans/plans.module';
 import { ReferralsModule } from './referrals/referrals.module';
@@ -24,8 +26,6 @@ import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 import { TasksModule } from './tasks/tasks.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { UsersModule } from './users/users.module';
-import { OrdersModule } from './orders/orders.module';
-import { CashoutModule } from './cashout/cashout.module';
 
 @Module({
 	imports: [
@@ -44,6 +44,9 @@ import { CashoutModule } from './cashout/cashout.module';
 					user: process.env.MAIL_USER,
 					pass: process.env.MAIL_PASS,
 				},
+			},
+			defaults: {
+				from: `"Step5th" <${process.env.MAIL_USER}>`,
 			},
 			template: {
 				dir: '../templates',
